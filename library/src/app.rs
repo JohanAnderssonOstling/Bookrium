679393cc5c71d04c84f3093ef6c450338d382219
johan <johan.andersson.ostling@protonmail.com>
2023-05-27 16:21:16 +0200

    Simplify library_app

--- library/src/app.rs
+++ library/src/app.rs
@@ -11,43 +11,32 @@ lazy_static!(
 type UUID = String;
 pub struct LibraryApp {
     // uuid as key
-    open_libraries: HashMap<UUID, LibraryModel>,
+    models: HashMap<UUID, LibraryModel>,
 }
 
 impl LibraryApp {
     /// Creates an instance of this struct with no open libraries
     pub fn new() -> Self {
-        Self {
-            open_libraries: HashMap::new(),
-        }
+        Self { models: HashMap::new() }
     }
-
 
     pub fn open(&mut self, uuid: &str, url: &str) {
-        self.open_libraries.insert(uuid.to_string(), LibraryModel::open(uuid));
+        self.models.insert(uuid.to_string(), LibraryModel::open(uuid));
     }
 
-
-    pub fn add_file(&mut self, uuid: &str, file: &MediaFile) -> Result<(), String> {
-        let library_model = self.open_libraries.get(uuid)
-            .ok_or("Library wasn't found")?;
+    pub fn add_file(&mut self, uuid: &str, file: &MediaFile){
+        let library_model = self.models.get(uuid).unwrap();
         RUNTIME.block_on(library_model.add_file(file));
-        Ok(())
     }
 
-    pub fn add_files(&mut self, uuid: &str, path: &str) -> Result<(), String> {
-        let library_model = self.open_libraries.get(uuid)
-            .ok_or("Library wasn't found")?;
+    pub fn add_files(&mut self, uuid: &str, path: &str){
+        let library_model = self.models.get(uuid).unwrap();
         RUNTIME.block_on(library_model.scan_library(path));
-
-        Ok(())
     }
 
 
-    pub fn get_files(&mut self, uuid: &str) -> Result<Vec<MediaFile>, String> {
-        let library_model = self.open_libraries.get(uuid)
-            .ok_or("Library wasn't found")?;
-        println!("Getting files from library app");
-        Ok(RUNTIME.block_on(library_model.fetch_files()))
+    pub fn get_files(&mut self, uuid: &str) -> Vec<MediaFile> {
+        let library_model = self.models.get(uuid).unwrap();
+        RUNTIME.block_on(library_model.fetch_files())
     }
 }
